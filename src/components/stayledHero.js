03f22fc039f47cdffbb86a4c7b3486b0266aceb2
Zoran Dasic <zorandsc@yahoo.com>
2019-09-03 20:49:50 +0200

    popravke male

--- src/components/stayledHero.js
+++ src/components/stayledHero.js
@@ -14,7 +14,7 @@ export default styled(stayledHero)`
   min-height: ${props => (props.home ? "calc(90vh)" : "60vh")};
   background: ${props =>
     props.home
-      ? "linear-gradient(rgba(210,170,92,0.5), rgba(0,0,0,0.9))"
+      ? "linear-gradient(rgba(210,170,92,0.5), rgba(0,0,0,1))"
       : "none"};
   background-position: center;
   background-attachment: fixed;
