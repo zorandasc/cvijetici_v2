8ea03ad383ad17b18b46ff3f4df1857f4e1186d4
Zoran Dasic <zorandsc@yahoo.com>
2019-09-08 00:58:12 +0200

    configmanifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -32,8 +32,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Svadbeni Cvet`,
+        short_name: `Svadbeni Cvet`,
         start_url: `/`,
         background_color: `#262626`,
         theme_color: `#d2aa5c`,
