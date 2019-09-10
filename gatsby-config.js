d000ba0bb565516478d2452891b4f245c1a4622d
Zoran Dasic <zorandsc@yahoo.com>
2019-09-10 18:40:51 +0200

    sc

--- gatsby-config.js
+++ gatsby-config.js
@@ -49,7 +49,7 @@ module.exports = {
         background_color: `#262626`,
         theme_color: `#d2aa5c`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/sc.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
