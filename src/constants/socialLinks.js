6bdb2717a9abaf9c876688c2d49bf2b9b6a8d80b
Zoran Dasic <zorandsc1@gmail.com>
2019-09-10 09:17:34 +0200

    socijal-utorak

--- src/constants/socialLinks.js
+++ src/constants/socialLinks.js
@@ -4,7 +4,8 @@ import { FaFacebook, FaPinterestSquare, FaInstagram } from "react-icons/fa"
 export default [
   {
     icon: <FaFacebook></FaFacebook>,
-    url: "https://facebook.com/?lang=en",
+    url:
+      "https://m.facebook.com/cveticizavasusavrsenusvadbu/?ref=bookmarks&mds=%2Fa%2Fpages%2Fchoose_photo_upload%2F%3FpageID%3D1471747599599619&",
   },
   {
     icon: <FaPinterestSquare></FaPinterestSquare>,
@@ -12,6 +13,6 @@ export default [
   },
   {
     icon: <FaInstagram></FaInstagram>,
-    url: "https://www.instagram.com/?lang=en",
+    url: "https://www.instagram.com/cvetici__za__svadbu/?igshid=1bs2j1shz10z",
   },
 ]
