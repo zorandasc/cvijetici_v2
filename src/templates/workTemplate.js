f6618b6dff33f2428408178593c5d7ae34784a86
Zoran Dasic <zorandsc1@gmail.com>
2019-09-13 16:17:30 +0200

    LIGHTBOX

--- src/templates/workTemplate.js
+++ src/templates/workTemplate.js
@@ -1,13 +1,29 @@
-import React from "react"
+import "@reach/dialog/styles.css"
+import React, { useState } from "react"
 import { graphql } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import StayledHero from "../components/stayledHero"
 import styles from "../css/template.module.css"
+import styled from "styled-components"
 import Img from "gatsby-image"
 import Title from "../components/title"
+import { Dialog } from "@reach/dialog"
+
+const PreviewButton = styled.div`
+  position: relative;
+  background: transparent;
+  border: none;
+  padding: 0;
+  margin: 0;
+`
 
 const workTemplate = ({ data }) => {
+  const [{ showLightbox, currentImage }, setLightbox] = useState({
+    showLightbox: false,
+    currentImage: null,
+  })
+
   const { prefiks, category, images } = data.work
   const [mainImage, ...workImages] = images
   return (
@@ -20,15 +36,40 @@ const workTemplate = ({ data }) => {
           <div className={styles.images}>
             {workImages.map((item, index) => {
               return (
-                <Img
-                  key={index}
-                  fluid={item.fluid}
-                  alt="svadbeni artikal"
-                  className={styles.image}
-                ></Img>
+                <PreviewButton
+                  onClick={() =>
+                    setLightbox({
+                      showLightbox: true,
+                      currentImage: item,
+                    })
+                  }
+                >
+                  <Img
+                    key={index}
+                    fluid={item.fluid}
+                    alt="svadbeni artikal"
+                    className={styles.image}
+                  ></Img>
+                </PreviewButton>
               )
             })}
           </div>
+          {showLightbox && (
+            <Dialog>
+              <Img fluid={currentImage.fluid} />
+              <button
+                type="button"
+                onClick={() =>
+                  setLightbox({
+                    showLightbox: false,
+                    currentImage: null,
+                  })
+                }
+              >
+                Close
+              </button>
+            </Dialog>
+          )}
         </div>
       </section>
     </Layout>
