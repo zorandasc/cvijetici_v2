987662a8ec4bdb89a519def8bcb80bf29f82bfb0
Zoran Dasic <zorandsc@yahoo.com>
2019-09-14 16:40:38 +0200

    navbar-scrolbar

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,12 +1,26 @@
-import React from "react"
+import React, { useState } from "react"
 import Navbar from "./navbar"
 import Footer from "./footer"
 import "./layout.css"
+import { useScrollPosition } from "@n8tb1t/use-scroll-position"
 
 const layout = ({ children }) => {
+  const [hideOnScroll, setHideOnScroll] = useState(true)
+
+  useScrollPosition(
+    ({ prevPos, currPos }) => {
+      const isShow = currPos.y > prevPos.y
+      if (isShow !== hideOnScroll) setHideOnScroll(isShow)
+    },
+    [hideOnScroll],
+    false,
+    false,
+    300
+  )
+
   return (
     <main>
-      <Navbar></Navbar>
+      <Navbar show={hideOnScroll}></Navbar>
       {children}
       <Footer></Footer>
     </main>
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -5,15 +5,16 @@ import socialLinks from "../constants/socialLinks"
 import AniLink from "gatsby-plugin-transition-link/AniLink"
 import { FaAlignRight } from "react-icons/fa"
 import logo from "../images/drawing.svg"
+import styled from "styled-components"
 
-const navbar = () => {
+const navbar = ({ className }) => {
   const [isOpen, setNav] = useState(false)
   const toggleNav = () => {
     setNav(() => !isOpen)
   }
 
   return (
-    <nav className={styles.navbar}>
+    <nav className={className}>
       <div className={styles.navCenter}>
         <div className={styles.navHeader}>
           <AniLink fade to="/">
@@ -69,4 +70,16 @@ const navbar = () => {
   )
 }
 
-export default navbar
+export default styled(navbar)`
+  position: fixed;
+  top: 0;
+  right: 0;
+  left: 0;
+  z-index: 1030;
+  background: var(--mainWhite);
+  box-shadow: 0 0.5rem 2rem rgba(0, 0, 0, 0.65);
+
+  visibility: ${props => (props.show ? "visible" : "hidden")};
+  transition: all 200ms ${props => (props.show ? "ease-in" : "ease-out")};
+  transform: ${props => (props.show ? "none" : "translate(0, -100%)")};
+`
