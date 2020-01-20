a18ec9fbabe4c8b35e96a5226dc0283cce56f01b
Zoran Dasic <zorandsc1@gmail.com>
2020-01-20 11:36:25 +0100

    Dodan freeepek <moja> na blog.js

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -4,8 +4,14 @@ import SEO from "../components/seo"
 import { graphql } from "gatsby"
 import StayledHero from "../components/stayledHero"
 import Blogs from "../components/Blogs/blogs"
-
 import SectionBcg from "../components/sectionBcg"
+import styled from "styled-components"
+
+const Moja = styled.a`
+  font-size: 0.8rem;
+  font-style: italic;
+  color: var(--mainBlack);
+`
 
 const blog = ({ data }) => {
   return (
@@ -14,6 +20,14 @@ const blog = ({ data }) => {
       <StayledHero img={data.blog.childImageSharp.fluid}></StayledHero>
       <SectionBcg img={data.rosBcg.childImageSharp.fluid}>
         <Blogs></Blogs>
+        <Moja
+          target="_blank"
+          rel="noopener noreferrer"
+          aria-label="freepik"
+          href="https://www.freepik.com/free-photos-vectors/mockup"
+        >
+          Mockup psd created by freepik - www.freepik.com
+        </Moja>
       </SectionBcg>
     </Layout>
   )
