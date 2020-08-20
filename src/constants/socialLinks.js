e84ac4efc367fab8a74ceb013d35f98889ff0ea1
Zoran Dasic <zorandsc@yahoo.com>
2020-08-20 13:39:50 +0200

    sidebar social icon

--- src/constants/socialLinks.js
+++ src/constants/socialLinks.js
@@ -1,36 +1,43 @@
 import React from "react"
-import { FaFacebook, FaPinterest, FaInstagram, FaYoutube } from "react-icons/fa"
+import {
+  FaFacebookSquare,
+  FaPinterestSquare,
+  FaInstagramSquare,
+  FaYoutubeSquare,
+} from "react-icons/fa"
 import styles from "../css/social.module.css"
 
 export default [
   {
     icon: (
-      <FaFacebook
+      <FaFacebookSquare
         className={`${styles.icon} ${styles.facebookIcon}`}
-      ></FaFacebook>
+      ></FaFacebookSquare>
     ),
     url:
       "https://m.facebook.com/cveticizavasusavrsenusvadbu/?ref=bookmarks&mds=%2Fa%2Fpages%2Fchoose_photo_upload%2F%3FpageID%3D1471747599599619&",
   },
   {
     icon: (
-      <FaPinterest
+      <FaPinterestSquare
         className={`${styles.icon} ${styles.pinterestIcon}`}
-      ></FaPinterest>
+      ></FaPinterestSquare>
     ),
     url: "https://www.pinterest.com/SvadbeniCvet/",
   },
   {
     icon: (
-      <FaInstagram
+      <FaInstagramSquare
         className={`${styles.icon} ${styles.instagramIcon}`}
-      ></FaInstagram>
+      ></FaInstagramSquare>
     ),
     url: "https://www.instagram.com/cvetici__za__svadbu/?igshid=1bs2j1shz10z",
   },
   {
     icon: (
-      <FaYoutube className={`${styles.icon} ${styles.youtubeIcon}`}></FaYoutube>
+      <FaYoutubeSquare
+        className={`${styles.icon} ${styles.youtubeIcon}`}
+      ></FaYoutubeSquare>
     ),
     url:
       "https://www.youtube.com/channel/UC86annL_cXXyT4AeKOv2N4A?view_as=subscriber",
